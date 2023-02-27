3aafd1395a9414df4ef06f2fc0e7e2a2c8335c62
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-02-27 11:12:21 +0100

    fix docker entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,8 @@
-composer install -n
-composer require api
+composer install --no-interaction --no-progress --no-suggest --no-scripts --prefer-dist --optimize-autoloader
 yarn install
 yarn run build
-bin/console doctrine:database:create -n
-bin/console doctrine:migrations:migrate -n
-bin/console doctrine:fixtures:load -n
+bin/console d:d:c -n
+bin/console d:m:m -n
+bin/console d:f:l -n
 chmod -R 777 /public/images
 exec apache2-foreground "$@"
