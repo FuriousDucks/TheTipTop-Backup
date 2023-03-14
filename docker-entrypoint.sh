c4395cea9b74379fad9dbd0457f6398710d58f3e
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-03-14 22:21:24 +0100

    Fix container var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@ yarn install
 yarn build
 php bin/console doctrine:database:create -n --if-not-exists
 php bin/console doctrine:migration:migrate -n --no-interaction
-php bin/console doctrine:fixture:load -n --no-interaction
+php bin/console doctrine:fixtures:load -n --no-interaction
 chmod -R 777 /public/images
 exec apache2-foreground "$@"
