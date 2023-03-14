a2f6899248b5c23ad2cfe914872e5a2f4cd56bd0
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-03-14 23:24:02 +0100

    App change

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 chmod -R 777 ./public/*
 composer install --no-interaction
-yarn install
-yarn build
-npm install
-npm run build
+yarn install --no-interaction
+yarn build --no-interaction
+npm install --no-interaction
+npm run build --no-interaction
 php bin/console doctrine:database:create -n --if-not-exists
 # php bin/console doctrine:migration:migrate -n --no-interaction
 # php bin/console doctrine:fixtures:load -n --no-interaction
