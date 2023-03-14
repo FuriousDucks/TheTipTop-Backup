bc2f8bd032a58e507c318dbcf2f9a3fd53d20976
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-03-14 15:04:18 +0100

    Fix container var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
-# composer install --no-interaction
-# yarn install
-# yarn build
-# bin/console d:d:c -n --if-not-exists
-# bin/console d:m:m -n --no-interaction
-# bin/console d:f:l -n --no-interaction
-# chmod -R 777 /public/images
+composer install --no-interaction
+yarn install
+yarn build
+php bin/console d:d:c -n --if-not-exists
+php bin/console d:m:m -n --no-interaction
+php bin/console d:f:l -n --no-interaction
+chmod -R 777 /public/images
 exec apache2-foreground "$@"
