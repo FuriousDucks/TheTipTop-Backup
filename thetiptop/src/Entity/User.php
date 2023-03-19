ad54e0f7e6b0807aff36d5ee8c4619a2cf4c7744
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-03-19 12:41:49 +0100

    Login

--- thetiptop/src/Entity/User.php
+++ thetiptop/src/Entity/User.php
@@ -2,57 +2,77 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cette adresse email')]
+#[ApiResource()]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['User:read', 'User:write'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: 'L\'adresse email est obligatoire')]
+    #[Assert\Email(message: 'L\'adresse email n\'est pas valide')]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $email = null;
 
     #[ORM\Column]
+    #[Groups(['User:read', 'User:write'])]
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message: 'Le mot de passe est obligatoire')]
+    #[Assert\Length(min: 8, minMessage: 'Le mot de passe doit faire au moins 8 caractères')]
+    #[Assert\Regex(pattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/', message: 'Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre et un caractère spécial')]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $password = null;
 
     #[ORM\Column(type: 'boolean')]
+    #[Groups(['User:read', 'User:write'])]
     private $isVerified = false;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $dateOfBirth = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $tel = null;
 
     #[ORM\Column(nullable: true, options: ['default' => 'CURRENT_TIMESTAMP'])]
+    #[Groups(['User:read', 'User:write'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(nullable: true, options: ['default' => 'CURRENT_TIMESTAMP'])]
+    #[Groups(['User:read', 'User:write'])]
     private ?\DateTimeImmutable $updatedAt = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['User:read', 'User:write'])]
     private ?string $facebookId = null;
 
     public function __construct()
