9f22b0bb7f113b8bfbc9c18777a98fbeba603f9d
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-04-11 15:13:06 +0200

    [Update] Jenkins

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,8 @@
 chmod -R 777 ./public/*
-composer install --no-interaction
-yarn install --no-interaction
-yarn build --no-interaction
-npm install --no-interaction
-npm run build --no-interaction
+composer install -n
+yarn install --force
+yarn build
 php bin/console doctrine:database:create -n --if-not-exists
-php bin/console doctrine:migration:migrate --no-interaction
-php bin/console doctrine:fixtures:load --no-interaction
+php bin/console doctrine:migration:migrate -n
+php bin/console doctrine:fixtures:load -n
 exec apache2-foreground "$@"
